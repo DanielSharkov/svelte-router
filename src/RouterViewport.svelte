08ff8ed3a7833da463acf4f5ae5aa685b0a13dd8
Daniel Sharkov <scharktv@gmail.com>
2019-07-07 00:07:20 +0200

    Add view in-/outro transition

--- src/RouterViewport.svelte
+++ src/RouterViewport.svelte
@@ -1,9 +1,46 @@
-<svelte:component
-	this={$router.route.component}
-	params={$router.route.params}
-	metadata={$router.route.metadata}
-/>
+{#if $router.route.component === current.component}
+	<div
+	transition:fade={{ duration: transitionDuration }}
+	on:introstart={event => {
+		introstart(event)
+	}}
+	on:introend={event => {
+		introend(event)
+	}}
+	on:outrostart={event => {
+		outrostart(event)
+	}}
+	on:outroend={event => {
+		updateComponent()
+		outroend(event)
+	}}>
+		<svelte:component
+			this={current.component}
+			params={current.params}
+			metadata={current.metadata}
+		/>
+	</div>
+{/if}
 
 <script>
+	import { fade } from 'svelte/transition'
+
 	export let router;
+	export let transitionDuration = 150
+	export let transitionFunc = fade
+	let current = {
+		component: null,
+		params: null,
+		metadata: null,
+	}
+
+	export let outrostart = function(){}
+	export let outroend = function(){}
+	export let introstart = function(){}
+	export let introend = function(){}
+
+	function updateComponent() {
+		setTimeout(() => current = $router.route)
+	}
+	updateComponent()
 </script>
