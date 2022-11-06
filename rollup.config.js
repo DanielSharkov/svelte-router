8ecf6e8e0286dc7cd0aae358f735407eb1fe0ab4
Daniel Sharkov <26609752+DanielSharkov@users.noreply.github.com>
2022-11-07 00:52:24 +0100

    refactor: upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json';
+import pkg from './package.json' assert {type: 'json'};
 
 export default {
 	input: pkg.source,
